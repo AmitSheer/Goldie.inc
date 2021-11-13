1695eae8b3e42f9cc961453dd263026d84e33c8c
neta-r <neta2611@walla.com>
2021-11-13 22:48:42 +0200

    neta for thje wimnnnnnn

---
+++ app/src/main/java/menu/menu.java
@@ -0,0 +1,16 @@
+package menu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.goldie.login.R;
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
