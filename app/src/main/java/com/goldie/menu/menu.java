070e3616e2e8a3001607ca1f9ad575dcc7f1c2c3
Amit-Sheer Cohen <amitcohen95@gmail.com>
2021-11-14 09:08:47 +0200

    fixed project structure

---
+++ app/src/main/java/com/goldie/menu/menu.java
@@ -0,0 +1,17 @@
+package com.goldie.menu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.goldie.R;
+
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
