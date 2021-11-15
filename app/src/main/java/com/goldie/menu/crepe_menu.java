3476f085ca05ac98b4067819fc4f0567d0f2fb0f
neta-r <neta2611@walla.com>
2021-11-15 21:12:20 +0200

    working buttons on main menu

---
+++ app/src/main/java/com/goldie/menu/crepe_menu.java
@@ -0,0 +1,15 @@
+package com.goldie.menu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.goldie.R;
+
+public class crepe_menu  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crepemenu);
+    }
+}
---
+++ app/src/main/java/com/goldie/menu/ice_cream_menu.java
@@ -0,0 +1,15 @@
+package com.goldie.menu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.goldie.R;
+
+public class ice_cream_menu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.icereammuneu);
+    }
+}
