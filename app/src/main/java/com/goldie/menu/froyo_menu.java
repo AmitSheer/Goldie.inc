cbece767f57799b7d3ed5d80e0d86351e431cf19
neta-r <neta2611@walla.com>
2021-11-16 20:55:08 +0200

    working btns

--- app/src/main/java/com/goldie/menu/froyo_menu.java
+++ app/src/main/java/com/goldie/menu/froyo_menu.java
@@ -1,6 +1,9 @@
 package com.goldie.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,13 @@ public class froyo_menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.froyomenu);
+        Button back_btn = (Button) findViewById(R.id.back);
+        back_btn.setOnClickListener(v -> openMenuPage());
     }
+
+    public void openMenuPage(){
+        Intent intent = new Intent(this,menu.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/goldie/menu/waffle_menu.java
+++ app/src/main/java/com/goldie/menu/waffle_menu.java
@@ -1,6 +1,8 @@
 package com.goldie.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,12 @@ public class waffle_menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wafflemenu);
+        Button back_btn = (Button) findViewById(R.id.back);
+        back_btn.setOnClickListener(v -> openMenuPage());
+    }
+
+    public void openMenuPage(){
+        Intent intent = new Intent(this,menu.class);
+        startActivity(intent);
     }
 }
