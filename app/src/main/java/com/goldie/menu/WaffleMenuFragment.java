8ceed7ab1044f45bf477e540bf174598a446630e
neta-r <neta2611@walla.com>
2021-11-21 20:24:31 +0200

    btn press is showing

--- app/src/main/java/com/goldie/menu/WaffleMenuFragment.java
+++ app/src/main/java/com/goldie/menu/WaffleMenuFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,8 @@ import com.goldie.R;
 
 public class WaffleMenuFragment extends Fragment {
 
+    ImageButton classic;
+
     public WaffleMenuFragment() {
         super(R.layout.fragment_waffle_menu);
     }
@@ -21,14 +24,34 @@ public class WaffleMenuFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
         super.onViewCreated(view, savedInstanceState);
+        classic = view.findViewById(R.id.waffleBtn);
+
+        classic.setOnClickListener(new View.OnClickListener() {
+            boolean checkActionOpen = false;
+            @Override
+            public void onClick(View view) {
+                if (!checkActionOpen) {
+                    checkActionOpen = true;
+                    classic.setSelected(true);
+                }
+                else {
+                    checkActionOpen = false;
+                    classic.setSelected(false);
+                }
+            }
+        });
+
 //        setContentView(R.layout.fragment_waffle_menu);
 //        Button back_btn = (Button) findViewById(R.id.back);
 //        back_btn.setOnClickListener(v -> openMenuPage());
-    }
+
 
 //    public void openMenuPage(){
 //        Intent intent = new Intent(this, MenuFragment.class);
 //        startActivity(intent);
 //    }
+
+    }
 }
